1c6b73dcb2ad8819ee432624123124a091bcd13f
Sri Devi Prasuna Bramhandam <sribramhandam@gmail.com>
2025-11-07 10:17:28 +0530

    Created poly.py
    Refactor polynomial representation using a doubly linked list. Update methods for inserting, appending, and adding polynomials.

--- poly.py
+++ poly.py
@@ -1,62 +1,73 @@
 class Node:
-    def __init__(self, coeff, exp):
-        self.coeff = coeff     # Coefficient of the term
-        self.exp = exp         # Exponent of the term
-        self.next = None       # Pointer to next node
-        self.prev = None       # Pointer to previous node
-class PolynomialDLL:
+    def __init__(self, coeff, power):
+        self.coeff = coeff
+        self.power = power
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
     def __init__(self):
         self.head = None
-def insert_term(self, coeff, exp):
-    new_node = Node(coeff, exp)
-    if not self.head:  # If list is empty
-        self.head = new_node
-        return
-    # Insert at the correct position
-    curr = self.head
-    prev = None
-    while curr and curr.exp > exp:
-        prev = curr
-        curr = curr.next
-    if curr and curr.exp == exp:
-        curr.coeff += coeff  # Add coefficients if exponent matches
-    else:
-        new_node.next = curr
-        new_node.prev = prev
-        if prev:
-            prev.next = new_node
+        self.tail = None
+
+    def append(self, coeff, power):
+        new_node = Node(coeff, power)
+        if not self.head:
+            self.head = self.tail = new_node
         else:
-            self.head = new_node
-        if curr:
-            curr.prev = new_node
-ef display(self):
-    node = self.head
-    result = ""
-    while node:
-        if node.coeff != 0:
-            sign = "+" if result else ""
-            result += f" {sign}{node.coeff}x^{node.exp}"
-        node = node.next
-    print(result.strip() or "0")
-def add(poly1, poly2):
-    result = PolynomialDLL()
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+
+    def traverse(self):
+        terms = []
+        temp = self.head
+        while temp:
+            terms.append(f"{temp.coeff}x^{temp.power}")
+            temp = temp.next
+        return " + ".join(terms) if terms else "0"
+
+def add_polynomials(poly1, poly2):
     p1 = poly1.head
     p2 = poly2.head
+    result = DoublyLinkedList()
     while p1 and p2:
-        if p1.exp > p2.exp:
-            result.insert_term(p1.coeff, p1.exp)
+        if p1.power == p2.power:
+            summed = p1.coeff + p2.coeff
+            if summed != 0:
+                result.append(summed, p1.power)
             p1 = p1.next
-        elif p2.exp > p1.exp:
-            result.insert_term(p2.coeff, p2.exp)
             p2 = p2.next
+        elif p1.power > p2.power:
+            result.append(p1.coeff, p1.power)
+            p1 = p1.next
         else:
-            result.insert_term(p1.coeff + p2.coeff, p1.exp)
-            p1 = p1.next
+            result.append(p2.coeff, p2.power)
             p2 = p2.next
+    # Add remaining terms
     while p1:
-        result.insert_term(p1.coeff, p1.exp)
+        result.append(p1.coeff, p1.power)
         p1 = p1.next
     while p2:
-        result.insert_term(p2.coeff, p2.exp)
+        result.append(p2.coeff, p2.power)
         p2 = p2.next
     return result
+
+# Example usage:
+if __name__ == "__main__":
+    # Polynomial 1: 5x^3 + 4x^2 + 2x^0
+    poly1 = DoublyLinkedList()
+    poly1.append(5, 3)
+    poly1.append(4, 2)
+    poly1.append(2, 0)
+    # Polynomial 2: 5x^2 + 5x^1 + 5x^0
+    poly2 = DoublyLinkedList()
+    poly2.append(5, 2)
+    poly2.append(5, 1)
+    poly2.append(5, 0)
+    result = add_polynomials(poly1, poly2)
+    print("Result:", result.traverse())
+
+  
+    
+       
